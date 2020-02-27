58a812be3fca56d8c40a0a9e556fd618fd700a34
Erik <halmos@users.noreply.github.com>
2020-02-27 10:25:36 -0500

    adds state manament to make the text area editable

--- src/components/kerning-editor.js
+++ src/components/kerning-editor.js
@@ -1,55 +1,77 @@
-import React, { useRef } from "react"
+import React, { useRef, useState, useEffect } from "react"
 import "./kerning-editor.css"
 
 const KerningEditor = () => {
   const textareaRef = useRef()
-  const spaceChar = `\u180e\u180e\u180e\u180e`; // mongolian seperator
-  //const spaceChar = `\u200A\u200D`
-  console.log('spaceChar', spaceChar, spaceChar.length)
-  const handleKeyPress = e => {
-    console.log(e.key);
+  const [text, setText] = useState(
+    `The Quick Brown Fox Jumped Over the Lazy dog.`
+  )
+  const [selectionStart, setSelectionStart] = useState(undefined)
+  const [selectionEnd, setSelectionEnd] = useState(undefined)
 
+  const spaceChar = `\u180e` // mongolian seperator
+  //const spaceChar = `\u200A\u200D`
+
+  const handleKeyPress = e => {
+    console.log("keyUp")
     const cursorPosition = textareaRef.current.selectionStart
     const currVal = textareaRef.current.value
 
-    console.log("cursor", cursorPosition)
     if (e.key === "." && e.getModifierState("Control")) {
       // increase spacing
-      console.log("Control-right")
       const newVal = `${currVal.substring(
         0,
         cursorPosition
       )}${spaceChar}${currVal.substring(cursorPosition)}`
-      textareaRef.current.value = newVal
-      textareaRef.current.selectionStart = cursorPosition
-      textareaRef.current.selectionEnd = cursorPosition
+
+      setText(newVal)
+      setSelectionStart(cursorPosition)
+      setSelectionEnd(cursorPosition)
     } else if (e.key === "," && e.getModifierState("Control")) {
       // reduce spacing
-      console.log("Control-left", currVal.substr(cursorPosition-spaceChar.length, spaceChar.length))
       if (currVal.substr(cursorPosition, spaceChar.length) === spaceChar) {
         // remove kerning spaces right of cursor
         const newVal = `${currVal.substring(
           0,
           cursorPosition
         )}${currVal.substring(cursorPosition + spaceChar.length)}`
-        textareaRef.current.value = newVal
-        textareaRef.current.selectionStart = cursorPosition
-        textareaRef.current.selectionEnd = cursorPosition
-      } else if (currVal.substr(cursorPosition - spaceChar.length, spaceChar.length) === spaceChar) {
+
+        setText(newVal)
+        setSelectionStart(cursorPosition)
+        setSelectionEnd(cursorPosition)
+      } else if (
+        currVal.substr(cursorPosition - spaceChar.length, spaceChar.length) ===
+        spaceChar
+      ) {
         // remove kerning spaces left of cursor
         const newVal = `${currVal.substring(
           0,
           cursorPosition - spaceChar.length
         )}${currVal.substring(cursorPosition)}`
-        textareaRef.current.value = newVal
-        textareaRef.current.selectionStart = cursorPosition - spaceChar.length
-        textareaRef.current.selectionEnd = cursorPosition - spaceChar.length
+        setText(newVal)
+        setSelectionStart(cursorPosition)
+        setSelectionEnd(cursorPosition)
       }
-    } else if(e.key ===  'ArrowLeft' ) {
-      // navigate left, skipping over space chars
-      // while (currVal.substr()) {}
+    } else {
+      setSelectionStart(undefined)
+      setSelectionEnd(undefined)
     }
   }
+
+  const handleChange = e => {
+    // this fires before keyUp event
+    console.log("change")
+    setText(e.target.value)
+  }
+
+  useEffect(() => {
+    if (selectionStart) {
+      textareaRef.current.selectionStart = selectionStart
+    }
+    if (selectionEnd) {
+      textareaRef.current.selectionEnd = selectionEnd
+    }
+  })
 
   return (
     <div className="kerning-editor">
@@ -57,10 +79,9 @@ const KerningEditor = () => {
         ref={textareaRef}
         className="kerning-editor__textarea"
         onKeyUp={e => handleKeyPress(e)}
-        value="T᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎h᠎᠎᠎᠎e qu᠎᠎᠎᠎i᠎᠎᠎᠎c᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎k br᠎᠎᠎᠎ow᠎᠎᠎᠎n f᠎᠎᠎᠎᠎᠎᠎᠎ox jump᠎᠎᠎᠎s o᠎᠎᠎᠎v᠎᠎᠎᠎e᠎᠎᠎᠎᠎᠎᠎᠎r t᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎he la᠎᠎᠎᠎z᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎᠎y d᠎᠎᠎᠎og."
-      >
-        
-      </textarea>
+        onChange={e => handleChange(e)}
+        value={text}
+      ></textarea>
     </div>
   )
 }
